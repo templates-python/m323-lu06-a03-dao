338d9577ea168d97a1ecd9b9ae53968836880646
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-28 14:29:00 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+from todoItem import TodoItem
+from todoDao import TodoDao
+
+def main():
+    # DAO-Instanz erstellen und Tabelle erstellen
+    dao = TodoDao('todo_example.db')
+    dao.create_table()
+
+    # Neues ToDo-Element hinzufügen
+    new_item = TodoItem(None, 'Buy milk', False)
+    dao.add_item(new_item)
+    new_item = TodoItem(None, 'Buy cheese', False)
+    dao.add_item(new_item)
+
+    # ToDo-Element abrufen
+    retrieved_item = dao.get_item(1)
+    if retrieved_item:
+        print(
+            f"Item ID: {retrieved_item.item_id}, Title: {retrieved_item.title}, Is Completed: {retrieved_item.is_completed}")
+
+    # Alle ToDo-Elemente abrufen
+    all_items = dao.get_all_items()
+    print("All items:")
+    for item in all_items:
+        print(f"Item ID: {item.item_id}, Title: {item.title}, Is Completed: {item.is_completed}")
+
+    # ToDo-Element aktualisieren
+    retrieved_item.is_completed = True
+    is_updated = dao.update_item(retrieved_item)
+    print(f"Was the item updated? {is_updated}")
+
+    # ToDo-Element löschen
+    is_deleted = dao.delete_item(1)
+    print(f"Was the item deleted? {is_deleted}")
+
+    # Verbindung schließen
+    dao.close()
 
 
+# Ausführung der Hauptlogik
 if __name__ == '__main__':
-    calculate()
+    main()
--- main_test.py
+++ main_test.py
@@ -1,6 +1,44 @@
-import main
+from todoItem import TodoItem
+from todoDao import TodoDao
 
-def test(capsys):
-    main.calculate()
-    captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+def test_add_item():
+    dao = TodoDao('test_todo.db')
+    dao.create_table()
+    new_item = TodoItem(None, 'Test item', False)
+    dao.add_item(new_item)
+    retrieved_item = dao.get_item(1)
+    assert retrieved_item is not None
+    assert retrieved_item.title == 'Test item'
+    assert retrieved_item.is_completed == False
+    dao.close()
+
+def test_get_item():
+    dao = TodoDao('test_todo.db')
+    retrieved_item = dao.get_item(1)
+    assert retrieved_item is not None
+    assert retrieved_item.title == 'Test item'
+    dao.close()
+
+def test_get_all_items():
+    dao = TodoDao('test_todo.db')
+    all_items = dao.get_all_items()
+    assert len(all_items) == 1
+    dao.close()
+
+def test_update_item():
+    dao = TodoDao('test_todo.db')
+    item_to_update = dao.get_item(1)
+    item_to_update.title = 'Updated item'
+    update_status = dao.update_item(item_to_update)
+    assert update_status == True
+    updated_item = dao.get_item(1)
+    assert updated_item.title == 'Updated item'
+    dao.close()
+
+def test_delete_item():
+    dao = TodoDao('test_todo.db')
+    delete_status = dao.delete_item(1)
+    assert delete_status == True
+    deleted_item = dao.get_item(1)
+    assert deleted_item is None
+    dao.close()
