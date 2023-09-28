8f3f3293b230eff26488264ffa137cfe8bcfdc68
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-28 14:59:08 +0200

    done

--- todoItem.py
+++ todoItem.py
@@ -3,6 +3,4 @@ from dataclasses import dataclass
 # TODO: Implementiere die TodoItem-Klasse mit @dataclass
 @dataclass
 class TodoItem:
-    item_id: int
-    title: str
-    is_completed: bool
+    ...
