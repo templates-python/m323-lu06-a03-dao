c5d41b5e2a125248f9f36de7348a8c3b062d6a14
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:07:35 +0200

    Ran pyink on branch main

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from todoItem import TodoItem
 from todoDao import TodoDao
 
+
 def main():
     # DAO-Instanz erstellen und Tabelle erstellen
-    dao = TodoDao('todo_example.db')
+    dao = TodoDao("todo_example.db")
     dao.create_table()
 
     # Neues ToDo-Element hinzufügen
-    new_item = TodoItem(None, 'Buy milk', False)
+    new_item = TodoItem(None, "Buy milk", False)
     dao.add_item(new_item)
-    new_item = TodoItem(None, 'Buy cheese', False)
+    new_item = TodoItem(None, "Buy cheese", False)
     dao.add_item(new_item)
 
     # ToDo-Element abrufen
     retrieved_item = dao.get_item(1)
     if retrieved_item:
         print(
-            f"Item ID: {retrieved_item.item_id}, Title: {retrieved_item.title}, Is Completed: {retrieved_item.is_completed}")
+            f"Item ID: {retrieved_item.item_id}, Title: {retrieved_item.title}, Is Completed: {retrieved_item.is_completed}"
+        )
 
     # Alle ToDo-Elemente abrufen
     all_items = dao.get_all_items()
     print("All items:")
     for item in all_items:
-        print(f"Item ID: {item.item_id}, Title: {item.title}, Is Completed: {item.is_completed}")
+        print(
+            f"Item ID: {item.item_id}, Title: {item.title}, Is Completed: {item.is_completed}"
+        )
 
     # ToDo-Element aktualisieren
     retrieved_item.is_completed = True
@@ -38,5 +42,5 @@ def main():
 
 
 # Ausführung der Hauptlogik
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
